fda17e65d1efbdb721ef1bbe54d4fca0522f018c
QKKQQK <zhouqiankang@hotmail.com>
2018-06-05 17:30:55 +0800

    加入随机生成代码及自动插入1000000行数据

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bson.json_util
 from datetime import datetime
+from datetime import timedelta
 
 # 默认localhost 27017
 client = MongoClient()
@@ -22,28 +23,30 @@ def insert():
 	post = {
 	  "name" : "手写风格",
 	  "flag" : random.randint(0, 1),
-	  "extid" : ObjectId("000000000000000000000000"),
+	  "extid" : object_id_from_datetime(),
 	  "exttype" : random.randint(1, 600),
 	  "type" : random.randint(1, 6) * 10,
 	  "tag" : [],
-	  "klist" : [ObjectId("5a0ab7dad5cb310b9830ef27"), ObjectId("5a0ab7dad5cb310b9830ef27")],
-	  "rlist" : [],
-	  "extlist" : [],
-	  "uid" : ObjectId("5a0ab7dad5cb310b9830ef27"),
+	  "klist" : [object_id_from_datetime() for _ in xrange(random.randint(0,3))],
+	  "rlist" : [object_id_from_datetime() for _ in xrange(random.randint(0,3))],
+	  "extlist" : [object_id_from_datetime() for _ in xrange(random.randint(0,3))],
+	  "uid" : object_id_from_datetime(),
 	  "uyear" : random.randint(2000, 2018),
 	  "date" : datetime.utcnow(),
-	  "pid" : ObjectId("5a0ab7dad5cb310b9830ef27"),
-	  "eid" : ObjectId("5a0ab7dad5cb310b9830ef27"),
+	  "pid" : object_id_from_datetime(),
+	  "eid" : object_id_from_datetime(),
 	  "v1" : random.uniform(10, 90),
 	  "v2" : random.uniform(50, 500),
 	  "v3" : random.uniform(200, 222222),
 	  "cfg" : "测试测试测试测试",
-	  "outid" : ObjectId("5a0ab7dad5cb310b9830ef27"),
+	  "outid" : object_id_from_datetime(),
 	  "_tick" : datetime.utcnow()
 	}
 	collection.insert_one(post)
 
+def object_id_from_datetime():
+    from_datetime = datetime.utcnow() + timedelta(days=random.randint(1, 10),hours=random.randint(1, 10),minutes=random.randint(0, 50),weeks=random.randint(1, 10))
+    return ObjectId.from_datetime(generation_time=from_datetime)
 
-
-for _ in xrange(10000):
+for _ in xrange(3000000):
 	insert()
