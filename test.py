acf10dc44a20698b10928a8f60d64f78beab2e55
QKKQQK <zhouqiankang@hotmail.com>
2018-06-05 15:54:59 +0800

    修改编码，改成UTF-8

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
+# encoding: utf-8
+
 import pymongo
+import pprint
 from pymongo import MongoClient
+
 
 # 默认localhost 27017
 client = MongoClient()
-
 # 选择 test_tbl db
 db = client['test_tbl']
-
 # 选择 tbl_report_raw_test 集合
 collection = db['tbl_report_raw_test']
+
+# 测试连接
+pprint.pprint(collection.find_one())
