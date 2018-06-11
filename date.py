2d8f0642b085aec87de446fac0128489e5abf833
QKKQQK <zhouqiankang@hotmail.com>
2018-06-11 09:25:27 +0800

    更新时间种类

--- date.py
+++ date.py
@@ -3,8 +3,16 @@ import time
 
 print("UTC Now: ", datetime.utcnow())
 print("UTC Now to Time: ", datetime.utcnow().time())
+print("UTC Now to Timestamp: ", time.mktime(datetime.utcnow().timetuple()))
+print("UTC from Timestamp: ", datetime.fromtimestamp(time.mktime(datetime.utcnow().timetuple())))
+
 print("Now: ", datetime.now())
 print("Now to Time: ", datetime.now().time())
+print("Now to Timestamp: ", time.mktime(datetime.now().timetuple()))
+
 print("Timestamp: ", time.time())
-print("UTC Datetime from Timestamp: ", datetime.fromtimestamp(time.time()))
+print("Timezone: ", time.timezone)
+print("UTC Timestamp: ", time.time() + time.timezone)
+print("UTC Datetime from Timestamp: ", datetime.utcfromtimestamp(time.time()))
 print("Datetime from Timestamp: ", datetime.fromtimestamp(time.time()))
+print("Datetime from UTC Timestamp: ", datetime.fromtimestamp(time.time() + time.timezone))
