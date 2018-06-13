fa2f9dbc64495d47649f711880441c6dae4c9cfa
QKKQQK <zhouqiankang@hotmail.com>
2018-06-13 17:20:34 +0800

    修改数据查询规则

--- 查询规则.py
+++ 查询规则.py
@@ -27,6 +27,16 @@
 who:	rlist, （extlist）, ugroup, uid, openid
 where:  fid, eid, (extlist)
 what:   name, exttype, type, tag, klist, (extlist)
-when:   (_id), (extlist), utc_date
+when:   (_id), (extlist), utc_date(y/m/d/h/m/s)
 how much: v1, v2, v3, (normalized)(v1, v2, v3), cfg
+
+[Stats]
 aggregate: sum, avg, min, max
+
+[Single Set Data Presentation]
+x axis: $group by date [y/m/d/h/m], fid, eid...
+y axis: v1, v2, v3, (normalized)(v1, v2, v3) 
+
+[Two Set Data Presentation]
+x axis [filter 1] : v1, v2, v3, (normalized)(v1, v2, v3) 
+y axis [filter 2] : v1, v2, v3, (normalized)(v1, v2, v3) 
